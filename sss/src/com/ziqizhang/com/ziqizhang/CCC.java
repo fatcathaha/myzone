a42962ab366f4aa36e7a48445f8e5fd0b4743f0b
fuck <fuck@qq.com>
2022-10-07 22:30:25 +0800

    提交ccc.java

---
+++ sss/src/com/ziqizhang/com/ziqizhang/CCC.java
@@ -0,0 +1,14 @@
+package com.ziqizhang.com.ziqizhang;
+
+/**
+ * ClassName:CCC
+ * Package:com.ziqizhang.com.ziqizhang
+ * Date:07/10/2022 9:14 PM
+ * Description:
+ * Author:1163070713@qq.com
+ */
+public class CCC {
+    public static void main(String[] args) {
+        System.out.println("哈哈哈");
+    }
+}
